6d0d5bc3772d7a1898398a2570934e45226d1c1b
Sahil Latif <sahillatif072@gmail.com>
2024-09-15 23:59:00 +0500

    updated task b1

--- Lab03/t_b1.cpp
+++ Lab03/t_b1.cpp
@@ -7,30 +7,40 @@ class Node{
         Node():val(0), prev(NULL), next(NULL){}
         Node(int v):val(v), prev(NULL), next(NULL){}
 };
-class DLL{
+class CDLL{
     public:
         Node* head;
-        DLL():head(NULL){}
+        CDLL():head(NULL){}
         void insertAtHead(int d){
             Node* newnode = new Node(d);
-            if(head==NULL)
+            if(head==NULL){
                 head = newnode;
+                newnode->next = head;
+                newnode->prev = newnode;
+            }
             else{
                 newnode->next = head;
+                newnode->prev = head->prev;
+                head->prev->next = newnode;
                 head->prev = newnode;
                 head = newnode;
             }
         }
         void insertAtEnd(int d){
             Node* newnode = new Node(d);
-            if(head==NULL)
+            if(head==NULL){
                 head = newnode;
+                newnode->next = head;
+                newnode->prev = newnode;
+            }
             else{
                 Node *temp = head;
-                while(temp->next!=NULL)
+                while(temp->next!=head)
                     temp = temp->next;
-                temp->next = newnode;
+                newnode->next = head;
                 newnode->prev = temp;
+                head->prev = newnode;
+                temp->next = newnode;
             }
         }
         void insertAtPos(int d, int p){
@@ -40,9 +50,9 @@ class DLL{
                 return;
             }
             Node *temp = head;
-            for(int i=0;i<p-1 && temp->next!=NULL;i++)
+            for(int i=0;i<p-1 && temp->next!=head;i++)
                 temp = temp->next;
-            if(temp->next==NULL)
+            if(temp->next==head)
                 insertAtEnd(d);
             else{
                 newnode->next = temp->next;
@@ -53,41 +63,43 @@ class DLL{
         }
         void printNodes(){
             Node* temp = head;
-            while(temp!=NULL){
+            while(temp->next!=head){
                 cout<<temp->val<<" ";
                 temp = temp->next;
             }
-            cout<<endl;
+            cout<<temp->val<<" continues same..."<<endl;
+
         }
-        void deleteAtPos(int p){
-            if(!head)
+        void deleteAtPos(int p) {
+            if (!head)
                 return;
-            if(p==0){
+            if (p == 0) {
                 Node* temp = head;
-                if(!(head->next))
+                if (head->next == head)
                     head = NULL;
-                else{
+                else {
+                    head->prev->next = head->next; 
+                    head->next->prev = head->prev;
                     head = head->next;
-                    head->prev = NULL;
                 }
                 delete temp;
                 return;
             }
             Node* temp = head;
-            for(int i=0;i<p && temp->next!=NULL;i++)
+            for (int i = 0; i < p && temp->next != head; i++) {
                 temp = temp->next;
-            Node *toDel = temp;
-            if(!(temp->next))
-                temp->prev->next = NULL;
-            else{
-                temp->prev->next = temp->next;
-                temp->next->prev = temp->prev;
             }
+            if (temp == head)
+                return;
+            Node* toDel = temp;
+            temp->prev->next = temp->next;
+            temp->next->prev = temp->prev;
+
             delete toDel;
-        }
+            }
 };
 int main() {
-    DLL list;
+    CDLL list;
     list.deleteAtPos(0);
     list.insertAtHead(1);
     list.insertAtHead(2);
